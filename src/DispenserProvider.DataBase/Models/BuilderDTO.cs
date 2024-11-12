1fef6364435050ad0aafd4536ba083728e4ced14
Stanislav Vysotskyi <ardenhide@gmail.com>
2024-11-12 15:47:04 +0300

    - mark as nullable

--- src/DispenserProvider.DataBase/Models/BuilderDTO.cs
+++ src/DispenserProvider.DataBase/Models/BuilderDTO.cs
@@ -5,8 +5,8 @@ public class BuilderDTO
     public int Id { get; set; }
     public decimal WithdrawalAmount { get; set; }
     public decimal RefundAmount { get; set; }
-    public DateTime StartTime { get; set; }
-    public DateTime FinishTime { get; set; }
+    public DateTime? StartTime { get; set; }
+    public DateTime? FinishTime { get; set; }
 
     public string ProviderAddress { get; set; }
 
