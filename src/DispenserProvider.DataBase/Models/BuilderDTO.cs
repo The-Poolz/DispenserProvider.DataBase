dd1432dc71dce5262c7cef20dab2b680c4d5a8a6
Stanislav Vysotskyi <ardenhide@gmail.com>
2024-11-12 15:47:52 +0300

    - cleanup

--- src/DispenserProvider.DataBase/Models/BuilderDTO.cs
+++ src/DispenserProvider.DataBase/Models/BuilderDTO.cs
@@ -3,12 +3,9 @@
 public class BuilderDTO
 {
     public int Id { get; set; }
-    public decimal WithdrawalAmount { get; set; }
-    public decimal RefundAmount { get; set; }
+    public decimal Amount { get; set; }
     public DateTime? StartTime { get; set; }
     public DateTime? FinishTime { get; set; }
-
-    public string ProviderAddress { get; set; }
 
     public int TransactionDetailId { get; set; }
     public virtual TransactionDetailDTO TransactionDetail { get; set; }
