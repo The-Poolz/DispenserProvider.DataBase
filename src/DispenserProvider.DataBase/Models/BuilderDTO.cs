73edfd54e2d17ef9418df7a22064258b80f1cf53
Stanislav Vysotskyi <ardenhide@gmail.com>
2024-11-12 17:25:17 +0300

    - remove UserDTO - update pk in `BuilderDTO`

--- src/DispenserProvider.DataBase/Models/BuilderDTO.cs
+++ src/DispenserProvider.DataBase/Models/BuilderDTO.cs
@@ -2,21 +2,11 @@
 
 public class BuilderDTO
 {
-    // PRIMARY KEY: Id
-    // FOREIGN KEY: UserAddress + ChainId + PoolId
-    // NOTE: Scheme like this we have relationship in 'DownloaderSettings' and 'DownloaderMapping', in 'DownloaderMapping' we use default auto-incremented integer ID.
-    // With this scheme we avoid duplicates by (UserAddress + ChainId + PoolId), but ID is simple auto-incremented integer ID.
-
-    public int Id { get; set; }
+    public string Id => $"HASH OF {TransactionDetailId}+{Amount}+{StartTime}+{FinishTime}";
     public decimal Amount { get; set; }
     public DateTime? StartTime { get; set; }
     public DateTime? FinishTime { get; set; }
 
-    // Inherit this data from TransactionDetail to avoid duplicates like in 'DownloaderMapping' table.
-    // IMPORTANT NOTE: We can simplify it, and store just TransactionDetailId, but need to create indexes in 'TransactionDetailDTO' and ID field.
-    // With this move we still can easily avoid duplicates, have fast search by indexes and no need to store 3 fields here (UserAddress + ChainId + PoolId), only TransactionDetailId
-    public string UserAddress { get; set; }
-    public long ChainId { get; set; }
-    public long PoolId { get; set; }
+    public long TransactionDetailId { get; set; }
     public virtual TransactionDetailDTO TransactionDetail { get; set; }
 }
--- src/DispenserProvider.DataBase/Models/TransactionDetailDTO.cs
+++ src/DispenserProvider.DataBase/Models/TransactionDetailDTO.cs
@@ -2,15 +2,14 @@
 
 public class TransactionDetailDTO
 {
-    // PRIMARY KEY: UserAddress + ChainId + PoolId
-    // FOREIGN KEY: UserAddress
+    public long Id { get; set; }
 
+    // Create index for UserAddress + ChainId + PoolId
+    public string UserAddress { get; set; }
     public long ChainId { get; set; }
     public long PoolId { get; set; }
-    public virtual List<BuilderDTO> Builders { get; set; }
 
-    public string UserAddress { get; set; }
-    public virtual UserDTO User { get; set; }
+    public virtual List<BuilderDTO> Builders { get; set; }
 
     public string? Signature { get; set; }
     public virtual SignatureDTO? UserSignature { get; set; }
