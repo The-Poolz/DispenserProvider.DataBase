e5f7138efc189a338d7a329013f85939e7993b6d
Stanislav Vysotskyi <ardenhide@gmail.com>
2024-11-12 16:34:36 +0300

    - add important note in `BuilderDTO`

--- src/DispenserProvider.DataBase/Models/BuilderDTO.cs
+++ src/DispenserProvider.DataBase/Models/BuilderDTO.cs
@@ -13,6 +13,8 @@ public class BuilderDTO
     public DateTime? FinishTime { get; set; }
 
     // Inherit this data from TransactionDetail to avoid duplicates like in 'DownloaderMapping' table.
+    // IMPORTANT NOTE: We can simplify it, and store just TransactionDetailId, but need to create indexes in 'TransactionDetailDTO' and ID field.
+    // With this move we still can easily avoid duplicates, have fast search by indexes and no need to store 3 fields here (UserAddress + ChainId + PoolId), only TransactionDetailId
     public string UserAddress { get; set; }
     public long ChainId { get; set; }
     public long PoolId { get; set; }
