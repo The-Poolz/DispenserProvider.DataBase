5edd59e58be57ffb5dd3e57090ddf695d08b2623
Stanislav Vysotskyi <ardenhide@gmail.com>
2024-12-24 13:02:16 +0300

    - add migration

--- src/DispenserProvider.Migrations/Migrations/20241224095922_Many-Signatures-To-One-Dispenser.Designer.cs
+++ src/DispenserProvider.Migrations/Migrations/20241224095922_Many-Signatures-To-One-Dispenser.Designer.cs
@@ -4,6 +4,7 @@ using DispenserProvider.DataBase;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DispenserProvider.DataBase.Migrations
 {
     [DbContext(typeof(DispenserContext))]
-    partial class DispenserContextModelSnapshot : ModelSnapshot
+    [Migration("20241224095922_Many-Signatures-To-One-Dispenser")]
+    partial class ManySignaturesToOneDispenser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
