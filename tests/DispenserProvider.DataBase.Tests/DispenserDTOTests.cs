7babc1fd40db61eb6d0e93e86aea81e9d50c8ba3
ArdenHide <ardenhide@gmail.com>
2025-03-17 15:30:25 +0300

    Unsafe method `GenerateId`

---
+++ tests/DispenserProvider.DataBase.Tests/DispenserDTOTests.cs
@@ -0,0 +1,41 @@
+using Xunit;
+using FluentAssertions;
+using Net.Web3.EthereumWallet;
+using DispenserProvider.DataBase.Models;
+
+namespace DispenserProvider.DataBase.Tests;
+
+public class DispenserDTOTests
+{
+    public class GenerateSourceForId
+    {
+        [Fact]
+        internal void WhenRefundIsNull_ShouldReturnsExpectedString()
+        {
+            var userAddress = EthereumAddress.ZeroAddress;
+            long withdrawChainId = 56;
+            long withdrawPoolId = 1;
+            long? refundChainId = null;
+            long? refundPoolId = null;
+
+            var result = DispenserDTO.GenerateSourceForId(userAddress, withdrawChainId, withdrawPoolId, refundChainId, refundPoolId);
+
+            result.Should().Be($"{userAddress}-{withdrawChainId}-{withdrawPoolId}");
+        }
+
+
+        [Fact]
+        internal void WhenRefundNotNull_ShouldReturnsExpectedString()
+        {
+            var userAddress = EthereumAddress.ZeroAddress;
+            long withdrawChainId = 56;
+            long withdrawPoolId = 1;
+            long? refundChainId = 97;
+            long? refundPoolId = 1;
+
+            var result = DispenserDTO.GenerateSourceForId(userAddress, withdrawChainId, withdrawPoolId, refundChainId, refundPoolId);
+
+            result.Should().Be($"{userAddress}-{withdrawChainId}-{withdrawPoolId}-{refundChainId}-{refundPoolId}");
+        }
+    }
+}
